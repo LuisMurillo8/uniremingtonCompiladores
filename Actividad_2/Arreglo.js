1524ea6351e991628db5d9415c083bc448963ede
Luis Murillo <luis.murillo@mawesi.net>
2024-05-19 15:20:22 -0500

    update arreglo.js

--- Actividad_2/Arreglo.js
+++ Actividad_2/Arreglo.js
@@ -1,16 +1,18 @@
 // Crear un arreglo con 4 elementos
-const miArreglo = ["elemento1", "elemento2", "elemento3", "elemento4"];
-
+const miArreglo = ["León", "Puma", "Leopardo", "Tigre"];
+console.log("A)")
+console.log("-Arreglo: [" + miArreglo + "]");
 // Recorrer el arreglo
 for (let i = 0; i < miArreglo.length; i++) {
-    console.log("Elemento " + (i + 1) + ": " + miArreglo[i]);
+    console.log("-Recorriendo el elemento " + (i + 1) + ": " + miArreglo[i]);
 }
 
 // Imprimir el último elemento
-console.log("El último elemento es: " + miArreglo[miArreglo.length - 1]);
+console.log("-El último elemento es: " + miArreglo[miArreglo.length - 1]);
+console.log("--------------------------------------------------");
 
 
-
+console.log("B)")
 // Crear un arreglo vacío para almacenar los números pares
 const numerosPares = [];
 
@@ -24,17 +26,19 @@ for (let i = 1; i < 10; i++) {
 }
 
 // Imprimir el arreglo de números pares
-console.log("Números pares menores que 10:", numerosPares);
-console.log("Contiene " + numerosPares.length + " elementos");
+console.log("-Números pares menores que 10:", numerosPares);
+console.log("-Contiene " + numerosPares.length + " elementos");
 
 
-const newNumero = [1,3,5,7,9]
+console.log("-Agregue 1 , 3 , 5 , 7 , 9 al final del array y muestrelo nuevamente")
+const newNumero = [1,3,5,7,9];
 const newNumeroPares = numerosPares.concat(newNumero);
 console.log(newNumeroPares);
 
 
 // Insertar el valor 20 en el vigésimo elemento del arreglo
+console.log("-inserte el valor 20 en el vigésimo elemento del array y muestrelo nuevamente.")
 numerosPares[19] = 20;
 
 // Mostrar el arreglo con el valor 20 insertado
-console.log("Arreglo con el valor 20 en el vigésimo elemento:", numerosPares);
+console.log(numerosPares);
